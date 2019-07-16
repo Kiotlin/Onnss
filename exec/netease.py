04a911c53a603260e854431155d0310a2feec45e
Kiotlin <719401217@qq.com>
2019-07-16 23:46:24 +0800

    Update netease.py

--- exec/netease.py
+++ exec/netease.py
@@ -14,12 +14,13 @@ import requests
 class NeteaseMusic:
     music_download_url = 'http://music.163.com/song/media/outer/url?id='
     playlist_url = 'https://music.163.com/playlist?id='
+    save_path = './download/'
 
     def __init__(self, **kwargs):
         self.keywords_dict = {}
         for k, v in kwargs.items():
             self.keywords_dict[k] = v
-        self.header = {
+        self.headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)\
             Chrome/69.0.3497.100 Safari/537.36',
             'referer': self.keywords_dict['url']
@@ -27,13 +28,22 @@ class NeteaseMusic:
         self.count = 0
 
     @property
-    def generate_song_list_id(self):
-        id = self.keywords_dict['url'].split('=')[-1]
-        return id
+    def __generate_song_list_id(self):
+        return self.keywords_dict['url'].split('=')[-1]
 
     def __crawl_html(self):
-        #....
-        return
+        file_path = f'{self.save_path}{self.__generate_song_list_id}.html'
+        if os.path.exists(file_path):
+            content = self.read_file(file_path)
+            print('read success!\n')
+            return content
+        else: # if not exist, cache in local
+            content = requests.get(url=self.keywords_dict.get('url'), headers=self.headers).text
+            self.write_file(file_path, 'a', content, encoding='utf-8')
+            print('write success!\n')
+            return content
+            
+        
     
     def write_file(self, file_name, mode, content, encoding=None):
         with open(file_name, mode, encoding=encoding) as f:
@@ -43,10 +53,19 @@ class NeteaseMusic:
         with open(file_name, 'r', encoding='utf-8') as f:
             return f.read()
 
+    def __analysis_node(self, html):
+        soup = bs(html, 'lxml')
+        
+        
+
+    def run(self):
+        content = self.__crawl_html()
+        self.__analysis_node(content)
+
 
 if __name__ == '__main__':
     url = input('input: \n')
     netease_music = NeteaseMusic(url=url)
-    print(netease_music.generate_song_list_id)
+    netease_music.run()
 
 
