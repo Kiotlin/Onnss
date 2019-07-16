764a1f83e12e0b98c515decd33bd50ca538cbf81
Kiotlin <719401217@qq.com>
2019-07-17 02:43:36 +0800

    Update netease.py

--- exec/netease.py
+++ exec/netease.py
@@ -42,8 +42,16 @@ class NeteaseMusic:
             self.write_file(file_path, 'a', content, encoding='utf-8')
             print('write success!\n')
             return content
-            
-        
+
+         
+    def delete_html(self):
+        pwd_path = os.path.abspath(self.save_path)
+        for root, dir, files in os.walk(pwd_path):
+            for file in files:
+                if file.endswith('.html'):
+                    file_path = os.path.join(root,file)
+                    os.remove(file_path)
+
     
     def write_file(self, file_name, mode, content, encoding=None):
         with open(file_name, mode, encoding=encoding) as f:
@@ -55,12 +63,36 @@ class NeteaseMusic:
 
     def __analysis_node(self, html):
         soup = bs(html, 'lxml')
+
+        song_list_names = soup.select('.f-ff2.f-brk')[0].get_text().strip().replace('\n', '')
+        self.song_list_name = re.sub(r'[\/:*?"<>|]', '-', song_list_names) # RegExp 
+        if not os.path.exists(f'{self.save_path}{self.song_list_name}'):
+            os.mkdir(f'{self.save_path}{self.song_list_name}')
         
+        ul_node = soup.find(name='ul', attrs={'class': 'f-hide'})
+        li_node = soup.find_all(name='li')
+        for a_node in li_node:
+            href_content = a_node.find('a').get('href')
+            song_name = a_node.find('a').get_text() 
+            yield {
+                'href_content': href_content,
+                'song_name': song_name
+            }
+
+    def __downloader(self, song_dict):
+        self.count += 1
+        song_id = song_dict.get('href_content').split('=')[-1]
+        song_name = song_dict.get('song_name').strip().replace(r'\n', '')
         
+        mp3_url = self.music_download_url + song_id + '.mp3'
+        mp3_stream = requests.get(url=mp3_url, headers=self.headers).content
+        mp3_file_name = f'{self.save_path}{self.song_list_name}/{song_name}.mp3'
+        self.write_file(mp3_file_name, 'wb', mp3_stream)
 
     def run(self):
-        content = self.__crawl_html()
-        self.__analysis_node(content)
+        # content = self.__crawl_html()
+        # self.__analysis_node(content)
+        self.delete_html()
 
 
 if __name__ == '__main__':
