9aadda49f9b7db26bc7efd304dcfa0cc6478f0ca
Kiotlin <719401217@qq.com>
2019-07-22 23:58:51 +0800

    fix qqmusic downloader

--- exec/qqmusic.py
+++ exec/qqmusic.py
@@ -10,14 +10,15 @@ import os
 import re
 import json
 from bs4 import BeautifulSoup as bs
-import requests
 from utility import utils
 from utility import config
+import requests
 
 class QQMusic:
     music_search_api = 'http://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp'
     token_url = 'https://c.y.qq.com/base/fcgi-bin/fcg_music_express_mobile3.fcg?'
     music_dl_url = 'http://ws.stream.qqmusic.qq.com/'
+    save_path = './download/'
 
     def __init__(self):
         self.keywords_dict = {}
@@ -26,28 +27,37 @@ class QQMusic:
         self.album_list = {}
         self.count = 0
 
-    def download(self, song_dict):
+    def downloader(self, song_dict):
         token_params = {
             'format': 'json205361747',
             'platform': 'yqq',
             'cid': '205361747',
             'songmid': song_dict['songmid'],
-            'filename': 'C400' + song_dict['songmid'] + '.mp3',
+            'filename': 'C400' + song_dict['songmid'] + '.m4a',
             'guid': '126548448'
         }
 
-        token_get = requests.get(url=token_url, params=token_params)
+        # 第一次请求获取vkey
+        token_get = requests.get(url=self.token_url, params=token_params).text
+        print(token_get)
         data = json.loads(token_get)
-        vkey = data['data']['items'].get('vkey')
+        vkey = ''
+        for item in data['data']['items']:
+            vkey = item.get('vkey')
+        if vkey == '': # 有些歌曲不开放下载
+            print("该歌曲不支持下载") 
+            
+        # 第二次请求获取mp3源
         mp3_params = {
             'fromtag': '0',
             'guid': '126548448',
             'vkey': vkey
         }
-        mp3_url = music_dl_url + token_params['filename']
+        mp3_url = self.music_dl_url + token_params['filename']
         mp3_stream = requests.get(url=mp3_url, params=mp3_params).content
-
-        return
+        mp3_file_name = self.save_path + song_dict['name'] + '.m4a'
+        utils.write_file(mp3_file_name, 'wb', mp3_stream)
+        
         
     def search_music(self, name) -> list:
         '''
@@ -87,4 +97,5 @@ class QQMusic:
 if __name__ == '__main__':
     qqmusic = QQMusic()
     search_list = qqmusic.search_music('稻香')
-    print(search_list)
+    for item in search_list:
+        qqmusic.downloader(item)
